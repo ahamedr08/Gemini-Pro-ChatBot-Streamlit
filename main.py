98dacf6307de7ab2891e401981717a4aaa858885
Ahamed R <134275920+ahamedr08@users.noreply.github.com>
2024-11-10 10:23:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ load_dotenv()
 # Configure Streamlit page settings
 st.set_page_config(
     page_title="Chat with Gemini-Pro!",
-    page_icon=":brain:",  # Favicon emoji
-    layout="centered",  # Page layout option
+    page_icon=":brain:",
+    layout="centered",
 )
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -21,25 +21,26 @@ model = gen_ai.GenerativeModel('gemini-pro')
 
 # Function to translate roles between Gemini-Pro and Streamlit terminology
 def translate_role_for_streamlit(user_role):
-    if user_role == "model":
-        return "assistant"
-    else:
-        return user_role
+    return "assistant" if user_role == "model" else user_role
 
 # Initialize chat session in Streamlit if not already present
 if "chat_session" not in st.session_state:
-    # Start the chat session with a structured welcome message
-    st.session_state.chat_session = model.start_chat(history=[
-        {"role": "assistant", "content": "Hello, and welcome to InterviewIQ. I’ll be your interviewer today, guiding you through technical and interpersonal questions aligned with the job description you provided. Please begin by introducing yourself."}
-    ])
+    st.session_state.chat_session = model.start_chat(history=[])
+    st.session_state.welcome_shown = False  # Flag to control welcome message display
 
 # Display the chatbot's title on the page
 st.title("🤖 InterviewIQ")
 
+# Display the welcome message if it's the start of the session
+if not st.session_state.welcome_shown:
+    with st.chat_message("assistant"):
+        st.markdown("Hello, and welcome to InterviewIQ. I’ll be your interviewer today, guiding you through technical and interpersonal questions aligned with the job description you provided. Please begin by introducing yourself.")
+    st.session_state.welcome_shown = True  # Ensure welcome message shows only once
+
 # Display the chat history
 for message in st.session_state.chat_session.history:
-    with st.chat_message(translate_role_for_streamlit(message["role"])):
-        st.markdown(message["content"])
+    with st.chat_message(translate_role_for_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
 
 # Input field for user's message
 user_prompt = st.chat_input("Ask AI...")
